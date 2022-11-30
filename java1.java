b3a934da1ce8ed84fb883ee15b8067ca562727d3
JeevaRaju <jeevarajuhari@gmail.com>
2022-11-30 19:52:31 +0530

    updated

--- java1.java
+++ java1.java
@@ -2,6 +2,8 @@ Public class Main
 {
 	public static void main(String[] args)
 	{
+		(notepad)
 		System.out.println("Naandhanda 1st");
+		System.out.println("idhan pa naa iniki idhanha  kathukiten");
 	}
 }
