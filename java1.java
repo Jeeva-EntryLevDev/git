78b48f42fb946e2772e7004a11658fad9e694c10
JeevaRaju <jeevarajuhari@gmail.com>
2022-11-30 21:19:02 +0530

    firstu pa

--- java1.java
+++ java1.java
@@ -3,7 +3,7 @@ Public class Main
 	public static void main(String[] args)
 	{
 
-
+                                System.out.println("copy copy copy copy");
 		System.out.println("Naandhanda headu enna pani vachirukanu pa");
 		System.out.println("Naandhanda 2nd");
 		System.out.println("Naandhanda 3rd");
