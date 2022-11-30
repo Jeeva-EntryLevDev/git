f2a22aa3f3a8556af131a35326e360a363404c74
Jeeva Raju <86867551+Nekkoa@users.noreply.github.com>
2022-11-30 19:33:36 +0530

    updated
    thambi i added new more lines

--- java1.java
+++ java1.java
@@ -2,6 +2,9 @@ Public class Main
 {
 	public static void main(String[] args)
 	{
+		System.out.println("Naandhanda headu enna pani vachirukanu pa");
+		System.out.println("Naandhanda 2nd");
+		System.out.println("Naandhanda 3rd");
 		System.out.println("Naandhanda 1st");
 	}
 }
